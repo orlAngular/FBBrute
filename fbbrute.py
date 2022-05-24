fc559de14e42b641a66ee2c26caadecfbe9cc9a7
orlAngular <106202790+orlAngular@users.noreply.github.com>
2022-05-24 18:21:23 -0500

    py correct

--- fbbrute.py
+++ fbbrute.py
@@ -27,9 +27,9 @@ __banner__ = """
 """
 
 print("[+] Facebook Brute Force\n")
-userid = raw_input("[*] Enter [Email|Phone|Username|ID]: ")
+userid = input("[*] Enter [Email|Phone|Username|ID]: ")
 try:
-	passlist = raw_input("[*] Set PATH to passlist: ")
+	passlist = input("[*] Set PATH to passlist: ")
 	if os.path.exists(passlist) != False:
 		print(__banner__)
 		print(" [+] Account to crack : {}".format(userid))
